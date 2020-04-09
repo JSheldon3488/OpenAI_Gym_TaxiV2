96343508dd4c80be46a2a93289c189b2c8f96f3e
Justin Sheldon <JSheldon3488@gmail.com>
2020-04-09 17:15:36 +0000

    Working Solution
    This solution is working but not quite good enough. Best average reward 9.559

--- agent.py
+++ agent.py
@@ -12,8 +12,10 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.epsilon = 1
 
-    def select_action(self, state):
+        
+    def select_action(self, state, episode_num):
         """ Given the state, select an action.
 
         Params
@@ -24,8 +26,17 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
-
+        #Update Epsilon based on the episode number
+        self.epsilon /= episode_num
+        
+        #Set up epsilon greedy policy
+        policy = np.ones(self.nA)*(self.epsilon/self.nA)
+        policy[np.argmax(self.Q[state])] += 1-self.epsilon
+        
+        #Select action based on epsilon greedy policy
+        return np.random.choice(np.arange(self.nA), p=policy)
+    
+    
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
 
@@ -37,4 +48,8 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        #Q-Learning off policy solution with constant learning rate 
+        alpha = 0.1
+        gamma = 1
+        self.Q[state][action] = (1-alpha)*self.Q[state][action] + alpha*(reward + gamma*np.max(self.Q[next_state]))
+        
