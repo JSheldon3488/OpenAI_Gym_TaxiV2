74b104f3f9ab6ec8a6a4035f34140fa8959442d6
Justin Sheldon <JSheldon3488@gmail.com>
2020-04-15 19:28:44 +0000

    Created 3 seperate agents
    Seperated the sarsa agent, expected sarsa agent, and the Q_Learning agent. Would like to compare them later.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from agent import Agent
+from agents import *
 from monitor import interact
 import gym
 import numpy as np
 
 env = gym.make('Taxi-v2')
-agent = Agent()
-avg_rewards, best_avg_reward = interact(env, agent)
+sarsa_agent = Sarsa_Agent()
+expected_sarsa_agent = Expected_Sarsa_Agent()
+qlearning_agent = QLearning_Agent()
+avg_rewards, best_avg_reward = interact(env, sarsa_agent)
+avg_rewards, best_avg_reward = interact(env, expected_sarsa_agent)
+avg_rewards, best_avg_reward = interact(env, qlearning_agent)
