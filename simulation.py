a37205c3fca36ba2f485f3b0b12a93f259042ec6
Justin Sheldon <JSheldon3488@gmail.com>
2020-04-16 00:44:59 -0500

    Added Expected Sarsa and QLearning Simulations

--- simulation.py
+++ simulation.py
@@ -20,27 +20,59 @@ def sarsa_simulation(env):
             #run simulation and save result (only need best average reward)
             _, best_avg_reward = interact(env, sarsa_agent)
             sim_results[sim] = best_avg_reward
-        
-        #End of sim for this alpha, average results and save
+        #End of sim for this alpha, average and store results
         average_rewards_per_alpha.append(np.mean(sim_results))
-        
-    
-    return alpha_grid, np.asarray(average_rewards_per_alpha)
-            
-            
 
+    return alpha_grid, np.asarray(average_rewards_per_alpha)
 
 #Create simulation to find good alpha for Expected Sarsa Agent
+def expected_sarsa_simulation(env):
+    num_sims = 20;
+    alpha_grid = np.arange(0.01, 0.25, 0.01)
+    average_rewards_per_alpha = []
+
+    for alpha in alpha_grid:
+        print(alpha)
+        sim_results = np.empty(num_sims)
+        for sim in range(num_sims):
+            # Create agent
+            expected_sarsa_agent = Expected_Sarsa_Agent(alpha)
+            # run simulation and save result (only need best average reward)
+            _, best_avg_reward = interact(env, expected_sarsa_agent)
+            sim_results[sim] = best_avg_reward
+        # End of sim for this alpha, average and store results
+        average_rewards_per_alpha.append(np.mean(sim_results))
+
+    return alpha_grid, np.asarray(average_rewards_per_alpha)
 
 #Create simulation to find good alpha for QLearning Agent
+def qlearning_simulation(env):
+    num_sims = 20;
+    alpha_grid = np.arange(0.01, 0.25, 0.01)
+    average_rewards_per_alpha = []
 
+    for alpha in alpha_grid:
+        print(alpha)
+        sim_results = np.empty(num_sims)
+        for sim in range(num_sims):
+            # Create agent
+            qlearning_agent = QLearning_Agent(alpha)
+            # run simulation and save result (only need best average reward)
+            _, best_avg_reward = interact(env, qlearning_agent)
+            sim_results[sim] = best_avg_reward
+        # End of sim for this alpha, average and store results
+        average_rewards_per_alpha.append(np.mean(sim_results))
+
+    return alpha_grid, np.asarray(average_rewards_per_alpha)
 
 #Function to display results of simulations
 def display_sim(modelName, grid, results):
+    #Set up plots
     fig, ax = plt.subplots()
     ax.plot(grid, results)
     ax.set(xlabel = "alpha", ylabel = "Average Reward", title = f"Finding best alpha for {modelName}")
     ax.grid()
+
     #Graph max point
     ymax = np.max(results)
     xmax = grid[np.argmax(results)]
@@ -50,11 +82,13 @@ def display_sim(modelName, grid, results):
     kw = dict(xycoords='data',textcoords="axes fraction",
               arrowprops=arrowprops, bbox=bbox_props, ha="right", va="top")
     ax.annotate(text, xy=(xmax, ymax), xytext=(0.94,0.96), **kw)
+
     #Set y lim so the annotation looks good
     ax.set_ylim(np.min(results), np.max(results)+0.1)
 
     #Save graph
     filepath = f"C:\Dev\Python\RL\Taxi_Problem\images\\{modelName}.png"
     fig.savefig(filepath)
+
     #Look at result
-    plt.show()
+    #plt.show()
